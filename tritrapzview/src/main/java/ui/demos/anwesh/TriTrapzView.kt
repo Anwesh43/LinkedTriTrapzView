3c43fdba3f2b51c9db06560d62027bfa9da1a734
Anwesh43 <anweshthecool0@gmail.com>
2020-11-26 02:20:56 +0530

    adding constants and libraries

--- tritrapzview/src/main/java/ui/demos/anwesh/TriTrapzView.kt
+++ tritrapzview/src/main/java/ui/demos/anwesh/TriTrapzView.kt
@@ -1,2 +1,25 @@
 package ui.demos.anwesh
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Path
+import android.graphics.RectF
+
+val parts : Int = 3
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 4.6f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#F44336",
+    "#673AB7",
+    "#4CAF50",
+    "#2196F3",
+    "#FF9800"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
