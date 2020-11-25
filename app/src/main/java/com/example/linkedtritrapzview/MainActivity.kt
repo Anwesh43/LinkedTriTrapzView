e602b58cf1c9ff7c9a15809219888070b213c49d
Anwesh43 <anweshthecool0@gmail.com>
2020-11-26 03:08:55 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/example/linkedtritrapzview/MainActivity.kt
+++ app/src/main/java/com/example/linkedtritrapzview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedtritrapzview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import ui.demos.anwesh.TriTrapzView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriTrapzView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
